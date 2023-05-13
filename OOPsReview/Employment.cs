25aa2fb4ea9639bbed0dd9f786964253654859c2
KINGSLEY <127166287+KingsleyWosu7@users.noreply.github.com>
2023-05-12 18:39:44 -0600

    Add project files.

---
+++ OOPsReview/Employment.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPsReview
+{
+    public class Employment
+    {
+        //fields
+        private SupervisoryLevel _level;
+        private string _Title;
+        private double _Years;
+
+        //properties
+        //are associated wth a single bunch of data
+        //does not have any value
+
+        public string Title
+        {
+            get { return _Title; }
+            
+            set
+            {
+                if()
+            }
+        }
+
+
+    }
+}
