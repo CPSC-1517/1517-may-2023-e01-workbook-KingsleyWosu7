10331f21f1e5252c16a4d7adfa83b4b30261b96b
KINGSLEY <127166287+KingsleyWosu7@users.noreply.github.com>
2023-05-24 18:22:10 -0600

    Sandbox

--- OOPsolutions/Sandbox/Program.cs
+++ OOPsolutions/Sandbox/Program.cs
@@ -13,3 +13,46 @@ Console.WriteLine(myHome.ToString());
 //to alter a value in the record instance, you must create a NEW instance
 myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
 Console.WriteLine(myHome.ToString());
+
+
+//Refactoring is the process of restruvting code, while not changing the original functionality.
+//The goal of refactoring is to improve internal code by making small changes without changing the external behaviour
+
+//original code 
+
+bool flag = false;
+if (myHome.Province.ToLower() == "ab")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "bc")
+{
+    flag = true;
+
+}
+if (myHome.Province.ToLower() == "sk")
+{
+    flag = true;
+}
+if (myHome.Province.ToLower() == "mn")
+{
+    flag = true;
+}
+
+//refactor using a switch statement
+switch (myHome.Province.ToLower())
+{
+    case "ab":
+    case "bc":
+    case "sk":
+    case "mn":
+        {
+            flag = true;
+            break;
+        }
+    default:
+        {
+            flag = false;
+            break;
+        }
+}
